dd2c69ed46f6658be4f898d752bccbc4c86d3126
HaeJi-Woo <whj1939@cu.ac.kr>
2019-04-02 16:34:42 +0900

    add

---
+++ HelloWorld/src/Excercise007.java
@@ -0,0 +1,19 @@
+
+public class Excercise007 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int max = 0;
+		int[] array = {1, 5, 3, 8, 2};
+		
+		for(int i = 0; i<4; i++) {
+			if (array[i] < array[i+1]) {
+				max = array[i+1];
+			}
+		}
+		
+		System.out.println("max: " + max);
+
+	}
+
+}

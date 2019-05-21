cd5865dbd23c67a67b8365e9e78e89897de5c80f
HaeJi-Woo <whj1939@cu.ac.kr>
2019-05-21 15:54:10 +0900

    add

---
+++ HelloWorld/src/RemoteControlExample.java
@@ -0,0 +1,12 @@
+
+public class RemoteControlExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RemoteControl rc;
+		rc = new Television();
+		rc = new Audio();
+
+	}
+
+}

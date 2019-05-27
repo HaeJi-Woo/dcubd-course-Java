02ae1c166777a10f379557e8ac2e2f5ae79693c0
HaeJi-Woo <whj1939@cu.ac.kr>
2019-05-27 16:12:27 +0900

    add

---
+++ HelloWorld/src/ch08/Soundable.java
@@ -0,0 +1,20 @@
+package ch08;
+
+public interface Soundable {
+	String sound();
+}
+
+class Cat implements Soundable {
+	@Override
+	public String sound() {
+		return "具克";
+	}
+
+}
+
+class Dog implements Soundable {
+	@Override
+	public String sound() {
+		return "港港";
+	}
+}

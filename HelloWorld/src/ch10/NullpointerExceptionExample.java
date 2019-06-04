778b82849be98b20c14560fd8158398ae2ad2268
HaeJi-Woo <whj1939@cu.ac.kr>
2019-06-04 15:47:19 +0900

    add

---
+++ HelloWorld/src/ch10/NullpointerExceptionExample.java
@@ -0,0 +1,12 @@
+package ch10;
+
+public class NullpointerExceptionExample {
+
+	public static void main(String[] args) {
+		String data = null;
+		System.out.println(data.toString());
+		
+
+	}
+
+}
